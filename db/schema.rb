206d2c33de43728afbd47c0b0006b9cebf760f00
egoruska85 <egorarbuzov@gmail.com>
2026-05-13 12:22:23 +0300

    add mail parametrs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_09_055847) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_13_080152) do
   create_table "abouts", force: :cascade do |t|
     t.string "name"
     t.string "surname"
@@ -53,6 +53,18 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_09_055847) do
   end
 
   create_table "logos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mailparametrs", force: :cascade do |t|
+    t.string "address"
+    t.integer "port"
+    t.string "domain"
+    t.string "username"
+    t.string "password"
+    t.string "authentication"
+    t.boolean "enable_starttls_auto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
